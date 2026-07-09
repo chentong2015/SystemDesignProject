c58340a83519443031017ef774ea4c2176bcfc61
chent5 <chen2015tong@gmail.com>
2026-07-09 18:11:56 +0200

    update rate limiting design

--- system-concurrency/rate-limiting/src/main/java/rate/limiter/RateLimiterFixWindow.java
+++ system-concurrency/rate-limiting/src/main/java/rate/limiter/RateLimiterFixWindow.java
@@ -1,6 +1,11 @@
 package rate.limiter;
 
-// Fixed Windows 固定时间范围的限流
+import java.util.HashMap;
+
+// Fixed Windows
+// 固定时间范围内接受有限的请求
+// 连续发送的请求timestamp时间戳一定是增加的
+//
 // --- window0 ----|-------- window1 ----------|
 // ---------------------------------------------
 // --O---O--O--K---O--O-----------O-------------
@@ -8,6 +13,59 @@ package rate.limiter;
 // 1 2 3 4 5 6 7 8 10 11 12 13 14 15 16 17 18 19
 public class RateLimiterFixWindow {
 
+    public static void main(String[] args) {
+        RateLimiter rateLimiter = new RateLimiter(10, 3);
+        System.out.println(rateLimiter.invokeRequest("chen", 1));
+        System.out.println(rateLimiter.invokeRequest("chen", 3));
+        System.out.println(rateLimiter.invokeRequest("chen", 7));
+        System.out.println(rateLimiter.invokeRequest("chen", 8)); // false
+        System.out.println(rateLimiter.invokeRequest("tong", 1));
+        System.out.println(rateLimiter.invokeRequest("tong", 5));
+        System.out.println(rateLimiter.invokeRequest("chen", 10)); // true
+    }
 
+     static class RateLimiter {
 
+         private int windSize;
+         private int maxRequestsPerWind;
+         private HashMap<String, WindowSlot> userSlotMap;
+
+         public RateLimiter(int windSize, int maxRequestsPerWind) {
+             this.windSize = windSize;
+             this.maxRequestsPerWind = maxRequestsPerWind;
+             this.userSlotMap = new HashMap<>();
+         }
+
+         // Time: O(1)
+         // Space: O(N) N is num of users
+         public boolean invokeRequest(String userId, int timestamp) {
+             if (userSlotMap.containsKey(userId)) {
+                 WindowSlot slot = userSlotMap.get(userId);
+                 if (timestamp < slot.startTimestamp + windSize) {
+                     // 属于同一个窗口区间内
+                     if (slot.count < maxRequestsPerWind) {
+                         slot.count++;
+                         return true;
+                     }
+                     return false;
+                 } else {
+                     // 超过时间戳后，移动到下一个窗口区间，重新统计
+                     slot.startTimestamp += windSize;
+                     slot.count = 1;
+                     return true;
+                 }
+             } else {
+                 WindowSlot slot = new WindowSlot();
+                 slot.count++;
+                 userSlotMap.put(userId, slot);
+                 return true;
+             }
+         }
+     }
+
+     // 记录当前区间内的统计
+     static class WindowSlot {
+         int count;
+         int startTimestamp;
+     }
 }
--- system-concurrency/rate-limiting/src/main/java/rate/limiter/RateLimiterSlidingWindow.java
+++ system-concurrency/rate-limiting/src/main/java/rate/limiter/RateLimiterSlidingWindow.java
@@ -1,6 +1,14 @@
 package rate.limiter;
 
-// Sliding Windows 滑动区间窗口的限流
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// Sliding Windows
+// 滑动区间窗口范围内接受有限请求
+// 连续发送的请求timestamp时间戳一定是增加的
+//
 // --- window0 ----|-------- window1 ----------|
 // ---------------------------------------------
 // --O---O--O--K---O--O-------------------O------
@@ -8,5 +16,45 @@ package rate.limiter;
 // 1 2 3 4 5 6 7 8 10 11 12 13 14 15 16 17 18 19
 public class RateLimiterSlidingWindow {
 
+    public static void main(String[] args) {
+        RateLimiter rateLimiter = new RateLimiter(10, 3);
+        System.out.println(rateLimiter.invokeRequest("chen", 1));
+        System.out.println(rateLimiter.invokeRequest("chen", 3));
+        System.out.println(rateLimiter.invokeRequest("chen", 7));
+        System.out.println(rateLimiter.invokeRequest("chen", 8)); // false
+        System.out.println(rateLimiter.invokeRequest("tong", 1));
+        System.out.println(rateLimiter.invokeRequest("tong", 5));
+        System.out.println(rateLimiter.invokeRequest("chen", 10)); // false
+        System.out.println(rateLimiter.invokeRequest("chen", 15));
+    }
 
+    static class RateLimiter {
+        private int windSize;
+        private int maxRequestsPerWind;
+        private HashMap<String, List<Integer>> userTimestampMap;
+
+        public RateLimiter(int windSize, int maxRequestsPerWind) {
+            this.windSize = windSize;
+            this.maxRequestsPerWind = maxRequestsPerWind;
+            this.userTimestampMap = new HashMap<>();
+        }
+
+        // TODO. 使用List存储请求的历史时间，动态往后滑动的同时移除在窗口外的时间
+        // Time: O(M)    M is max num request in window
+        // Space: O(N*M) 每个用户都必须存储的请求时间
+        public boolean invokeRequest(String userId, int timestamp) {
+            List<Integer> timestampList = userTimestampMap.getOrDefault(userId, new ArrayList<>());
+            timestampList = timestampList.stream()
+                    .filter(t -> (timestamp - windSize) < t)
+                    .collect(Collectors.toList());
+
+            if (timestampList.size() < maxRequestsPerWind) {
+                timestampList.add(timestamp);
+                userTimestampMap.put(userId, timestampList);
+                return true;
+            }
+            // 如果已经达到最大请求数目，拒绝添加
+            return false;
+        }
+    }
 }
